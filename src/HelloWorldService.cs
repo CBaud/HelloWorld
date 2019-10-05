280f3d308382d13d8faa317ea307f55d2983c4ac
Chris Baudin <cbaud@microsoft.com>
2019-10-05 13:40:36 -0700

    Initialize repository with hello world application.

---
+++ src/HelloWorldService.cs
@@ -0,0 +1,30 @@
+﻿namespace HelloWorld
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public sealed class HelloWorldService : IHostedService
+    {
+        private readonly ILogger _logger;
+
+        public HelloWorldService(ILogger<HelloWorldService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Hello!");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Goodbye!");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace HelloWorld
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var hostBuilder = CreateHostBuilder(args);
+            using var host = hostBuilder.Build();
+            await host.StartAsync();
+            await host.StopAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) => new HostBuilder()
+            .ConfigureAppConfiguration((configurationBuilder) =>
+            {
+                configurationBuilder
+                    .AddCommandLine(args)
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureLogging((loggingBuilder) =>
+            {
+                loggingBuilder.
+                    AddConsole();
+            })
+            .ConfigureServices((services) =>
+            {
+                services
+                    .AddHostedService<HelloWorldService>()
+                    .Configure<ConsoleLifetimeOptions>((options) => options.SuppressStatusMessages = true);
+            });
+    }
+}
