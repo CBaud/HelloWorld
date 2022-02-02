454010707494a4c686ce6ba111b0463dfe75a92a
Chris Baudin <cbaud@microsoft.com>
2022-02-02 11:23:36 -0800

    Migrate to minimal API

--- src/service/Program.cs
+++ src/service/Program.cs
@@ -1,27 +1,42 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-using Project;
+using Project.HealthChecks;
+using Project.Http;
+using Project.Options;
+using Project.Services;
 
-var hostBuilder = new HostBuilder()
-    .ConfigureAppConfiguration((configurationBuilder) =>
-    {
-        configurationBuilder
-            .AddCommandLine(args)
-            .AddEnvironmentVariables();
-    })
-    .ConfigureLogging((loggingBuilder) =>
-    {
-        loggingBuilder.
-            AddConsole();
-    })
-    .ConfigureWebHost((webHostBuilder) =>
-    {
-        webHostBuilder
-            .UseKestrel()
-            .UseStartup<Startup>();
-    });
-using var host = hostBuilder.Build();
-await host.RunAsync();
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .AddCommandLine(args)
+    .AddEnvironmentVariables();
+
+builder.Logging.AddConsole();
+
+builder.Services
+    .AddHostedService<DiagnosticListenerService>()
+    .AddHostedService<HelloWorldService>()
+    .AddHttpContextAccessor()
+    .AddSingleton<IHttpContextFactory, CustomHttpContextFactory>()
+    .Configure<ConsoleLifetimeOptions>((options) => options.SuppressStatusMessages = true)
+    .ConfigureOptions<KestrelServerOptionsSetup>();
+
+builder.Services
+    .AddHealthChecks();
+
+builder.Services
+    .AddControllers();
+
+var application = builder.Build();
+
+application.MapControllers();
+application.MapHealthChecks("/health/keepalive", KeepAliveHealthCheckOptions.Instance);
+application.MapHealthChecks("/health/readiness", ReadinessHealthCheckOptions.Instance);
+application.MapHealthChecks("/health/startup", StartupHealthCheckOptions.Instance);
+
+await application.RunAsync();
